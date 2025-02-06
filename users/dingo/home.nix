06f2befe6c6e56b6f1c433cfbc1731eb78d9bee8
Petr Dio <dingos@seznam.cz>
2025-02-06 09:04:52 +0100

    Commit z 06-02-2025 09:04:52

--- users/dingo/home.nix
+++ users/dingo/home.nix
@@ -20,6 +20,7 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
+     pkgs.nfs-utils
      pkgs.hello
      pkgs.hello
      pkgs.wget
